ced936f1ed84f0c985aade8850a227e478bc593d
acash132 <125028527+acash132@users.noreply.github.com>
2026-04-23 12:25:24 +0530

    Application State Management Added

--- Global.asax.cs
+++ Global.asax.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
-namespace MasterPages
+namespace WebApplication2
 {
-    public class Global : HttpApplication
+    public class Global : System.Web.HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+
+        protected void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["UserCount"] = 0;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application["UserCount"] = (int)Application["UserCount"] + 1;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Application["UserCount"] = (int)Application["UserCount"] - 1;
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Numberofusers.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Numberofusers.aspx.cs" Inherits="WebApplication2.Numberofusers" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div>
+            <asp:Label ID="lblCount" runat="server" Text="Count"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
