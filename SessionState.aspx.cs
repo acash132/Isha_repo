027577b0858133c96964dc94e5ca71c366658f64
acash132 <125028527+acash132@users.noreply.github.com>
2026-04-23 10:58:18 +0530

    SessionState Added

---
+++ SessionState.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class SessionState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["ClicksCount"] == null)
+            {
+                Session["ClicksCount"] = 0;
+            }
+            lblCount.Text = "ClicksCount: " + Session["ClicksCount"].ToString();
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int count = (int)Session["ClicksCount"];
+            count++;
+            Session["ClicksCount"] = count;
+            lblCount.Text = "ClicksCount: " + Session["ClicksCount"].ToString();
+        }
+    }
+}
